57001c5554f7ca54e47af68fdfa51f699043711a
beatrixkh <beatrixh@uw.edu>
2022-05-12 11:15:58 -0700

    cleaning up init file

--- src/vivarium_census_prl_synth_pop/components/__init__.py
+++ src/vivarium_census_prl_synth_pop/components/__init__.py
@@ -1,4 +1,2 @@
-from vivarium_census_prl_synth_pop.components.population import (
-    Population
-)
+from vivarium_census_prl_synth_pop.components.population import Population
 from vivarium_census_prl_synth_pop.components.migration.household import HouseholdMigration
