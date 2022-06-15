92c8e401173703ed8e4a5eef684e31398c3e16d9
beatrixkh <beatrixh@uw.edu>
2022-06-15 15:03:49 -0700

    bugfix

--- src/vivarium_census_prl_synth_pop/components/population.py
+++ src/vivarium_census_prl_synth_pop/components/population.py
@@ -125,7 +125,7 @@ class Population:
 
         # add typing
         chosen_persons['age'] = chosen_persons['age'].astype('float64')
-        chosen_persons['state'] = chosen_persons['age'].astype('int64')
+        chosen_persons['state'] = chosen_persons['state'].astype('int64')
         # chosen_persons['alive'] = pd.Categorical(chosen_persons['alive'], categories=['alive', 'dead'])
         for col in ['relation_to_household_head', 'sex', 'race_ethnicity']:
             chosen_persons[col] = chosen_persons[col].astype('category')
