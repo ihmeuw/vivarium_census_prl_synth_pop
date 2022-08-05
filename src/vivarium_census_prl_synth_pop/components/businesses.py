15d474801d3ffeedaac6828e7b5f5c79dc83c046
beatrixkh <beatrixh@uw.edu>
2022-08-05 12:17:27 -0700

    addressing PR comments

--- src/vivarium_census_prl_synth_pop/components/businesses.py
+++ src/vivarium_census_prl_synth_pop/components/businesses.py
@@ -14,10 +14,10 @@ class Businesses:
     IMPROVE DESCRIPTION
 
     on init:
-        assign everyone 18 and up an employer
+        assign everyone of working age an employer
 
     on timestep:
-        new job if turning 18
+        new job if turning working age
         change jobs at rate of 50 changes per 100 person years
 
     FROM ABIE:  please use a skewed distribution for the business sizes:
@@ -62,15 +62,15 @@ class Businesses:
 
     def on_initialize_simulants(self, pop_data: SimulantData) -> None:
         """
-        Assign everyone 18 and older an employer
+        Assign everyone working age and older an employer
         """
         if pop_data.creation_time < self.start_time:
             self.businesses = self.generate_businesses(pop_data)
 
             pop = self.population_view.subview(['age', 'tracked']).get(pop_data.index)
-            pop['employer_id'] = -1
-            over_17 = pop.loc[pop.age >= data_values.WORKING_AGE].index
-            pop.loc[over_17, 'employer_id'] = self.assign_random_employer(over_17)
+            pop['employer_id'] = -1  # we're using -1 for employer_id of unemployed
+            working_age = pop.loc[pop.age >= data_values.WORKING_AGE].index
+            pop.loc[working_age, 'employer_id'] = self.assign_random_employer(working_age)
 
             # merge on employer addresses and names
             pop = pop.merge(
@@ -80,7 +80,7 @@ class Businesses:
             )
 
             # handle untracked sims
-            pop.loc[pop.tracked == False, 'employer_id'] = -2
+            pop.loc[pop.tracked == False, 'employer_id'] = -2  # using -2 for employer_id of untracked people
             pop.loc[pop.tracked == False, 'employer_name'] = 'NA'
             pop.loc[pop.tracked == False, 'employer_address'] = 'NA'
             self.population_view.update(
@@ -89,7 +89,7 @@ class Businesses:
         else:
             new_births = self.population_view.get(pop_data.index)
 
-            new_births["employer_id"] = -1
+            new_births["employer_id"] = -1  # we're using -1 for employer_id of unemployed
             new_births["employer_name"] = 'unemployed'
             new_births["employer_address"] = 'NA'
 
@@ -97,13 +97,13 @@ class Businesses:
 
     def on_time_step(self, event: Event):
         """
-        assign job if turning 18
+        assign job if turning working age
         change jobs at rate of 50 changes / 100 person-years
         """
 
         # change jobs
         pop = self.population_view.subview(self.columns_created + ['age']).get(event.index)
-        employed = pop.loc[pop.employer_id > -1].index
+        employed = pop.loc[pop.employer_id != -1].index
         changing_jobs = self.randomness.filter_for_rate(
             employed,
             np.ones(len(employed))*(data_values.YEARLY_JOB_CHANGE_RATE * event.step_size.days / utilities.DAYS_PER_YEAR)
@@ -119,19 +119,19 @@ class Businesses:
                 self.businesses.set_index("employer_id")['employer_name'].to_dict()
             )
 
-        # assign job if turning 18
-        turned_18 = pop.loc[
+        # assign job if turning working age
+        turning_working_age = pop.loc[
             (pop.age >= data_values.WORKING_AGE - event.step_size.days / utilities.DAYS_PER_YEAR) &
             (pop.age < data_values.WORKING_AGE)
             ].index
-        if len(turned_18) > 0:
-            pop.loc[turned_18, 'employer_id'] = self.assign_random_employer(turned_18)
+        if len(turning_working_age) > 0:
+            pop.loc[turning_working_age, 'employer_id'] = self.assign_random_employer(turning_working_age)
 
             # add employer addresses and names
-            pop.loc[turned_18, "employer_address"] = pop.loc[turned_18, "employer_id"].map(
+            pop.loc[turning_working_age, "employer_address"] = pop.loc[turning_working_age, "employer_id"].map(
                 self.businesses.set_index("employer_id")['employer_address'].to_dict()
             )
-            pop.loc[turned_18, "employer_name"] = pop.loc[turned_18, "employer_id"].map(
+            pop.loc[turning_working_age, "employer_name"] = pop.loc[turning_working_age, "employer_id"].map(
                 self.businesses.set_index("employer_id")['employer_name'].to_dict()
             )
 
@@ -145,13 +145,16 @@ class Businesses:
 
     def generate_businesses(self, pop_data: SimulantData) -> pd.DataFrame():
         pop = self.population_view.subview(['age']).get(pop_data.index)
-        over_17 = pop.loc[pop.age >= data_values.WORKING_AGE]
+        working_age = pop.loc[pop.age >= data_values.WORKING_AGE]
 
-        n_employed = len(over_17)
+        n_employed = len(working_age)
         employee_counts = np.random.lognormal(
             4, 1, size=int(n_employed // data_values.EXPECTED_EMPLOYEES_PER_BUSINESS)
         ).round()
         n_businesses = len(employee_counts)
+        # TODO: note: this fixed number of employers based on population size is not adaptive.
+        #  we don't (necessarily?) expect the number of work-eligible people to stay constant over time.
+        #  Consider updating.
         businesses = pd.DataFrame({
             'employer_id': np.arange(n_businesses),
             'employer_name': ['not implemented']*n_businesses,
@@ -160,14 +163,14 @@ class Businesses:
         })
 
         unemployed = pd.DataFrame({
-            'employer_id': [-1],
+            'employer_id': [-1],  # we're using -1 for employed_id of unemployed
             'employer_name': ['unemployed'],
             'employer_address': ['NA'],
             'probability': 0, #TODO: implement unemployment
         })
 
         untracked = pd.DataFrame({
-            'employer_id': [-2],
+            'employer_id': [-2],  # we're using -2 for employer_id of untracked people
             'employer_name': ['NA'],
             'employer_address': ['NA'],
             'probability': 0,
