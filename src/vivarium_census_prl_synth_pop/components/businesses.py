aec8731b3e1eab05b07d4d6026928efaec8b39dd
Beatrix Haddock <57412727+beatrixkh@users.noreply.github.com>
2022-08-18 16:13:14 -0700

    ran black (#45)

--- src/vivarium_census_prl_synth_pop/components/businesses.py
+++ src/vivarium_census_prl_synth_pop/components/businesses.py
@@ -9,7 +9,11 @@ from vivarium.framework.time import get_time_stamp
 from vivarium_public_health import utilities
 
 from vivarium_census_prl_synth_pop.constants import data_values, data_keys, metadata
-from vivarium_census_prl_synth_pop.constants.data_values import UNEMPLOYED_ID, UNTRACKED_ID, WORKING_AGE
+from vivarium_census_prl_synth_pop.constants.data_values import (
+    UNEMPLOYED_ID,
+    UNTRACKED_ID,
+    WORKING_AGE,
+)
 
 
 class Businesses:
@@ -31,7 +35,7 @@ class Businesses:
     """
 
     def __repr__(self) -> str:
-        return 'Businesses()'
+        return "Businesses()"
 
     ##############
     # Properties #
@@ -49,8 +53,13 @@ class Businesses:
         self.start_time = get_time_stamp(builder.configuration.time.start)
         self.location = builder.data.load(data_keys.POPULATION.LOCATION)
         self.randomness = builder.randomness.get_stream(self.name)
-        self.columns_created = ['employer_id', 'employer_name', 'employer_address', 'employer_zipcode']
-        self.columns_used = ['age', 'tracked'] + self.columns_created
+        self.columns_created = [
+            "employer_id",
+            "employer_name",
+            "employer_address",
+            "employer_zipcode",
+        ]
+        self.columns_used = ["age", "tracked"] + self.columns_created
         self.population_view = builder.population.get_view(self.columns_used)
         self.businesses = None
 
@@ -63,11 +72,11 @@ class Businesses:
         self.businesses_move_rate = builder.value.register_rate_producer(
             f"{self.name}.move_rate", source=move_rate_data
         )
-        self.addresses = builder.components.get_component('Address')
+        self.addresses = builder.components.get_component("Address")
         builder.population.initializes_simulants(
             self.on_initialize_simulants,
-            requires_columns=['age'],
-            creates_columns=self.columns_created
+            requires_columns=["age"],
+            creates_columns=self.columns_created,
         )
         builder.event.register_listener("time_step", self.on_time_step)
 
@@ -82,33 +91,29 @@ class Businesses:
         if pop_data.creation_time < self.start_time:
             self.businesses = self.generate_businesses(pop_data)
 
-            pop = self.population_view.subview(['age', 'tracked']).get(pop_data.index)
-            pop['employer_id'] = UNEMPLOYED_ID
+            pop = self.population_view.subview(["age", "tracked"]).get(pop_data.index)
+            pop["employer_id"] = UNEMPLOYED_ID
             working_age = pop.loc[pop.age >= data_values.WORKING_AGE].index
-            pop.loc[working_age, 'employer_id'] = self.assign_random_employer(working_age)
+            pop.loc[working_age, "employer_id"] = self.assign_random_employer(working_age)
 
             # merge on employer addresses and names
             pop = pop.merge(
-                self.businesses[self.columns_created],
-                on='employer_id',
-                how='left'
+                self.businesses[self.columns_created], on="employer_id", how="left"
             )
 
             # handle untracked sims
-            pop.loc[~pop.tracked, 'employer_id'] = UNTRACKED_ID
-            pop.loc[~pop.tracked, 'employer_name'] = 'NA'
-            pop.loc[~pop.tracked, 'employer_address'] = 'NA'
-            pop.loc[~pop.tracked, 'employer_zipcode'] = 'NA'
-            self.population_view.update(
-                pop
-            )
+            pop.loc[~pop.tracked, "employer_id"] = UNTRACKED_ID
+            pop.loc[~pop.tracked, "employer_name"] = "NA"
+            pop.loc[~pop.tracked, "employer_address"] = "NA"
+            pop.loc[~pop.tracked, "employer_zipcode"] = "NA"
+            self.population_view.update(pop)
         else:
             new_births = self.population_view.get(pop_data.index)
 
             new_births["employer_id"] = UNEMPLOYED_ID
-            new_births["employer_name"] = 'unemployed'
-            new_births["employer_address"] = 'NA'
-            new_births["employer_zipcode"] = 'NA'
+            new_births["employer_name"] = "unemployed"
+            new_births["employer_address"] = "NA"
+            new_births["employer_zipcode"] = "NA"
 
             self.population_view.update(new_births)
 
@@ -118,11 +123,13 @@ class Businesses:
         change jobs at rate of 50 changes / 100 person-years
         businesses change addresses at rate of 10 changes / 100 person-years
         """
-        pop = self.population_view.subview(self.columns_created + ['age']).get(event.index)
+        pop = self.population_view.subview(self.columns_created + ["age"]).get(event.index)
 
-        all_businesses = self.businesses.loc[~self.businesses['employer_id'].isin([UNEMPLOYED_ID, UNTRACKED_ID])]
+        all_businesses = self.businesses.loc[
+            ~self.businesses["employer_id"].isin([UNEMPLOYED_ID, UNTRACKED_ID])
+        ]
         businesses_that_move = self.addresses.determine_if_moving(
-            all_businesses['employer_id'], self.businesses_move_rate
+            all_businesses["employer_id"], self.businesses_move_rate
         )
 
         if len(businesses_that_move) > 0:
@@ -137,7 +144,7 @@ class Businesses:
                 address_map=address_map,
                 zipcode_map=zipcode_map,
                 address_col_name="employer_address",
-                zipcode_col_name="employer_zipcode"
+                zipcode_col_name="employer_zipcode",
             )
 
             # update employer address and zipcode in the pop table
@@ -145,115 +152,141 @@ class Businesses:
             pop = self.addresses.update_address_and_zipcode(
                 df=pop,
                 rows_to_update=rows_changing_addresses.index,
-                id_key=rows_changing_addresses['employer_id'],
+                id_key=rows_changing_addresses["employer_id"],
                 address_map=address_map,
                 zipcode_map=zipcode_map,
                 address_col_name="employer_address",
-                zipcode_col_name="employer_zipcode"
+                zipcode_col_name="employer_zipcode",
             )
 
         # change jobs if of working age already
         working_age = pop.loc[pop.age >= WORKING_AGE].index
         changing_jobs = self.randomness.filter_for_rate(
             working_age,
-            self.job_change_rate(working_age) * event.step_size.days / utilities.DAYS_PER_YEAR
+            self.job_change_rate(working_age)
+            * event.step_size.days
+            / utilities.DAYS_PER_YEAR,
         )
         if len(changing_jobs) > 0:
-            pop.loc[changing_jobs, "employer_id"] = self.assign_different_employer(changing_jobs)
+            pop.loc[changing_jobs, "employer_id"] = self.assign_different_employer(
+                changing_jobs
+            )
             pop = self._update_employer_metadata(pop, changing_jobs)
 
         # assign job if turning working age
         turning_working_age = pop.loc[
-            (pop.age >= data_values.WORKING_AGE - event.step_size.days / utilities.DAYS_PER_YEAR) &
-            (pop.age < data_values.WORKING_AGE)
-            ].index
+            (
+                pop.age
+                >= data_values.WORKING_AGE - event.step_size.days / utilities.DAYS_PER_YEAR
+            )
+            & (pop.age < data_values.WORKING_AGE)
+        ].index
         if len(turning_working_age) > 0:
-            pop.loc[turning_working_age, 'employer_id'] = self.assign_random_employer(turning_working_age)
+            pop.loc[turning_working_age, "employer_id"] = self.assign_random_employer(
+                turning_working_age
+            )
             pop = self._update_employer_metadata(pop, turning_working_age)
 
-        self.population_view.update(
-            pop
-        )
+        self.population_view.update(pop)
 
     ##################
     # Helper methods #
     ##################
 
     def generate_businesses(self, pop_data: SimulantData) -> pd.DataFrame():
-        pop = self.population_view.subview(['age']).get(pop_data.index)
+        pop = self.population_view.subview(["age"]).get(pop_data.index)
         n_working_age = len(pop.loc[pop.age >= data_values.WORKING_AGE])
 
         # TODO: when have more known employers, maybe move to csv
-        known_employers = pd.DataFrame({
-            'employer_id': [UNEMPLOYED_ID],
-            'employer_name': ['unemployed'],
-            'employer_address': ['NA'],
-            'prevalence': [1 - data_values.PROPORTION_WORKFORCE_EMPLOYED[self.location]],
-        })
+        known_employers = pd.DataFrame(
+            {
+                "employer_id": [UNEMPLOYED_ID],
+                "employer_name": ["unemployed"],
+                "employer_address": ["NA"],
+                "prevalence": [1 - data_values.PROPORTION_WORKFORCE_EMPLOYED[self.location]],
+            }
+        )
 
-        pct_adults_needing_employers = 1 - known_employers['prevalence'].sum()
+        pct_adults_needing_employers = 1 - known_employers["prevalence"].sum()
         n_need_employers = np.round(n_working_age * pct_adults_needing_employers)
         employee_counts = np.random.lognormal(
             4, 1, size=int(n_need_employers // data_values.EXPECTED_EMPLOYEES_PER_BUSINESS)
         ).round()
         n_businesses = len(employee_counts)
-        random_employers = pd.DataFrame({
-            'employer_id': np.arange(n_businesses),
-            'employer_name': ['not implemented']*n_businesses,
-            'prevalence': employee_counts / employee_counts.sum() * pct_adults_needing_employers,
-        })
-        address_assignments = self.addresses.generate(
-                random_employers.index,
-                state=metadata.US_STATE_ABBRV_MAP[self.location].lower()
+        random_employers = pd.DataFrame(
+            {
+                "employer_id": np.arange(n_businesses),
+                "employer_name": ["not implemented"] * n_businesses,
+                "prevalence": employee_counts
+                / employee_counts.sum()
+                * pct_adults_needing_employers,
+            }
         )
-        random_employers['employer_address'] = random_employers.index.map(address_assignments['address'])
-        random_employers['employer_zipcode'] = random_employers.index.map(address_assignments['zipcode'])
+        address_assignments = self.addresses.generate(
+            random_employers.index, state=metadata.US_STATE_ABBRV_MAP[self.location].lower()
+        )
+        random_employers["employer_address"] = random_employers.index.map(
+            address_assignments["address"]
+        )
+        random_employers["employer_zipcode"] = random_employers.index.map(
+            address_assignments["zipcode"]
+        )
 
-        untracked = pd.DataFrame({
-            'employer_id': [UNTRACKED_ID],
-            'employer_name': ['NA'],
-            'employer_address': ['NA'],
-            'employer_zipcode': ['NA'],
-            'prevalence': 0,
-        })
+        untracked = pd.DataFrame(
+            {
+                "employer_id": [UNTRACKED_ID],
+                "employer_name": ["NA"],
+                "employer_address": ["NA"],
+                "employer_zipcode": ["NA"],
+                "prevalence": 0,
+            }
+        )
 
-        businesses = pd.concat([known_employers, random_employers, untracked], ignore_index=True)
+        businesses = pd.concat(
+            [known_employers, random_employers, untracked], ignore_index=True
+        )
         return businesses
 
-    def assign_random_employer(self, sim_index: pd.Index, additional_seed: Any = None) -> pd.Series:
+    def assign_random_employer(
+        self, sim_index: pd.Index, additional_seed: Any = None
+    ) -> pd.Series:
         return self.randomness.choice(
             index=sim_index,
-            choices=self.businesses['employer_id'],
-            p=self.businesses['prevalence'],
-            additional_key=additional_seed
+            choices=self.businesses["employer_id"],
+            p=self.businesses["prevalence"],
+            additional_key=additional_seed,
         )
 
     def assign_different_employer(self, changing_jobs: pd.Index) -> pd.Series:
-        current_employers = self.population_view.subview(['employer_id']).get(changing_jobs)['employer_id']
+        current_employers = self.population_view.subview(["employer_id"]).get(changing_jobs)[
+            "employer_id"
+        ]
 
         new_employers = current_employers.copy()
         additional_seed = 0
         while (current_employers == new_employers).any():
-            unchanged_employers = (current_employers == new_employers)
+            unchanged_employers = current_employers == new_employers
             new_employers[unchanged_employers] = self.assign_random_employer(
                 sim_index=new_employers[unchanged_employers].index,
-                additional_seed=additional_seed
+                additional_seed=additional_seed,
             )
             additional_seed += 1
 
         return new_employers
 
-    def _update_employer_metadata(self, pop: pd.DataFrame, rows_to_update: pd.Index) -> pd.DataFrame:
+    def _update_employer_metadata(
+        self, pop: pd.DataFrame, rows_to_update: pd.Index
+    ) -> pd.DataFrame:
         employer_ids = pop.loc[rows_to_update, "employer_id"]
 
         pop.loc[rows_to_update, "employer_address"] = employer_ids.map(
-            self.businesses.set_index("employer_id")['employer_address'].to_dict()
+            self.businesses.set_index("employer_id")["employer_address"].to_dict()
         )
         pop.loc[rows_to_update, "employer_zipcode"] = employer_ids.map(
-            self.businesses.set_index("employer_id")['employer_zipcode'].to_dict()
+            self.businesses.set_index("employer_id")["employer_zipcode"].to_dict()
         )
         pop.loc[rows_to_update, "employer_name"] = employer_ids.map(
-            self.businesses.set_index("employer_id")['employer_name'].to_dict()
+            self.businesses.set_index("employer_id")["employer_name"].to_dict()
         )
 
         return pop
--- src/vivarium_census_prl_synth_pop/constants/data_values.py
+++ src/vivarium_census_prl_synth_pop/constants/data_values.py
@@ -37,16 +37,14 @@ PROBABILITY_OF_SPACE_IN_NAME = {
 #####################
 
 # data from https://www.jec.senate.gov/public/index.cfm/republicans/fl/
-PROPORTION_WORKFORCE_EMPLOYED = {
-    'Florida': .576
-}
+PROPORTION_WORKFORCE_EMPLOYED = {"Florida": 0.576}
 
 WORKING_AGE = 18
 EXPECTED_EMPLOYEES_PER_BUSINESS = 90.105203
 
-YEARLY_JOB_CHANGE_RATE = .5  # 50 changes per 100 py
+YEARLY_JOB_CHANGE_RATE = 0.5  # 50 changes per 100 py
 
 UNEMPLOYED_ID = -1
 UNTRACKED_ID = -2
 
-BUSINESS_MOVE_RATE_YEARLY = .1 # 10 changes per 100 py
+BUSINESS_MOVE_RATE_YEARLY = 0.1  # 10 changes per 100 py
