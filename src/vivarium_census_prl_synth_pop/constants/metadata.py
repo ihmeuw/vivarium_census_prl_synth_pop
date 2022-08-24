b737898ac6418915f14c24522b072b6576be7257
beatrixkh <beatrixh@uw.edu>
2022-08-24 11:21:04 -0700

    updating artifact

--- src/vivarium_census_prl_synth_pop/constants/metadata.py
+++ src/vivarium_census_prl_synth_pop/constants/metadata.py
@@ -97,7 +97,7 @@ PERSONS_COLUMNS_MAP = {
     "RAC1P": "race",
 }
 
-SUBSET_PERSONS_COLUMNS_MAP = {"ST": "state", "SERIALNO": "census_household_id"}
+SUBSET_PERSONS_COLUMNS_MAP = {'ST': 'state', 'SERIALNO': 'census_household_id', 'PWGTP' :'person_weight'}
 
 LATINO_VAR_MAP = {i: (1 if i == 1 else 0) for i in range(1, 25)}
 
--- src/vivarium_census_prl_synth_pop/data/loader.py
+++ src/vivarium_census_prl_synth_pop/data/loader.py
@@ -12,7 +12,6 @@ for an example.
 
    No logging is done here. Logging is done in vivarium inputs itself and forwarded.
 """
-from pathlib import Path
 from typing import Dict
 
 import pandas as pd
@@ -147,7 +146,14 @@ def load_households(key: str, location: str) -> pd.DataFrame:
     persons = load_raw_persons_data(metadata.SUBSET_PERSONS_COLUMNS_MAP, location)
 
     # subset data to household ids in person file
-    data = data.query(f"census_household_id in {list(persons.census_household_id.unique())}")
+    data = data.query(f"census_household_id in {list(persons['census_household_id'].unique())}")
+
+    # merge on person weights for GQ
+    data = data.merge(
+        persons.loc[['GQ' in i for i in persons.census_household_id]],
+        on='census_household_id',
+        how='left'
+    )
 
     return data
 
