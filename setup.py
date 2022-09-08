037036e8bb4abaf5de37b18c38678e88411b8f86
Beatrix Haddock <57412727+beatrixkh@users.noreply.github.com>
2022-09-08 11:35:33 -0700

    running black on files outside of src/ dir (#53)

--- setup.py
+++ setup.py
@@ -17,53 +17,41 @@ if __name__ == "__main__":
         long_description = f.read()
 
     install_requirements = [
-        'vivarium==0.10.12',
-        'vivarium_public_health==0.10.17',
-
-        'click',
-        'gbd_mapping>=3.0.0, <4.0.0',
-        'jinja2',
-        'loguru',
-        'numpy',
-        'pandas',
-        'scipy',
-        'tables',
-        'pyyaml',
-        'faker==13.6.0',
+        "vivarium==0.10.12",
+        "vivarium_public_health==0.10.17",
+        "click",
+        "gbd_mapping>=3.0.0, <4.0.0",
+        "jinja2",
+        "loguru",
+        "numpy",
+        "pandas",
+        "scipy",
+        "tables",
+        "pyyaml",
+        "faker==13.6.0",
     ]
 
     # use "pip install -e .[dev]" to install required components + extra components
-    extras_require = [
-        'vivarium_cluster_tools==1.3.0',
-        'vivarium_inputs[data]==4.0.6',
-    ]
+    extras_require = ["vivarium_cluster_tools==1.3.0", "vivarium_inputs[data]==4.0.6"]
 
     setup(
-        name=about['__title__'],
-        version=about['__version__'],
-
-        description=about['__summary__'],
+        name=about["__title__"],
+        version=about["__version__"],
+        description=about["__summary__"],
         long_description=long_description,
-        license=about['__license__'],
+        license=about["__license__"],
         url=about["__uri__"],
-
         author=about["__author__"],
         author_email=about["__email__"],
-
-        package_dir={'': 'src'},
-        packages=find_packages(where='src'),
+        package_dir={"": "src"},
+        packages=find_packages(where="src"),
         include_package_data=True,
-
         install_requires=install_requirements,
-        extras_require={
-            'dev': extras_require,
-        },
-
+        extras_require={"dev": extras_require},
         zip_safe=False,
-
-        entry_points='''
+        entry_points="""
             [console_scripts]
             make_artifacts=vivarium_census_prl_synth_pop.tools.cli:make_artifacts
             make_results=vivarium_census_prl_synth_pop.tools.cli:make_results
-        '''
+        """,
     )
