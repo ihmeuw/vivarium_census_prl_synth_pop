22e8f404616f300725440fb701681a20eea893d3
albrja <albrja@uw.edu>
2022-10-26 13:08:58 -0700

    Fixing header name

--- src/vivarium_census_prl_synth_pop/data/generate_business_names.py
+++ src/vivarium_census_prl_synth_pop/data/generate_business_names.py
@@ -31,8 +31,12 @@ def generate_business_names_data(n_total_names: str):
         new_names = pd.concat([new_names, more_names]).drop_duplicates()
         new_names = new_names.loc[~new_names.isin(real_but_uncommon_names)]
 
-    new_names = pd.Series(data=new_names, name="business_names")
-    new_names.to_csv(paths.BUSINESS_NAMES_DATA_ARTIFACT_INPUT_PATH, compression="bz2", index=False)
+    new_names.to_csv(
+        paths.BUSINESS_NAMES_DATA_ARTIFACT_INPUT_PATH,
+        header=["business_names"],
+        index=False,
+        compression="bz2"
+    )
 
 
 def make_bigrams(df: pd.DataFrame):
