55f87dba96583721ee1cba8d91c7078d2c655424
Zeb Burke-Conte <zmbc@uw.edu>
2022-12-15 16:42:23 -0800

    Respond to PR comments

--- src/vivarium_census_prl_synth_pop/components/person.py
+++ src/vivarium_census_prl_synth_pop/components/person.py
@@ -3,8 +3,9 @@ from typing import Dict
 import pandas as pd
 from vivarium.framework.engine import Builder
 from vivarium.framework.event import Event
-from vivarium.framework.values import Pipeline
 from vivarium.framework.randomness import RESIDUAL_CHOICE
+from vivarium.framework.utilities import from_yearly
+from vivarium.framework.values import Pipeline
 
 from vivarium_census_prl_synth_pop.components.synthetic_pii import (
     update_address_and_zipcode,
@@ -52,20 +53,32 @@ class PersonMigration:
         ]
         self.population_view = builder.population.get_view(self.columns_needed)
 
-        move_rates_data = builder.lookup.build_table(
-            data=pd.read_csv(
-                paths.INDIVIDUAL_DOMESTIC_MIGRATION_RATES_PATH,
-            ),
+        move_rates_data = pd.read_csv(
+            paths.INDIVIDUAL_DOMESTIC_MIGRATION_RATES_PATH,
+        )
+
+        value_columns = [
+            "gq_person_migration_rate",
+            "new_household_migration_rate",
+            "non_reference_person_migration_rate",
+        ]
+        move_rates_data[value_columns] = from_yearly(
+            move_rates_data[value_columns],
+            pd.Timedelta(days=builder.configuration.time.step_size),
+        )
+        move_rates_data = move_rates_data.rename(
+            columns=lambda c: c.replace("_migration_rate", "")
+        ).assign(no_move=RESIDUAL_CHOICE)
+
+        move_probabilities_lookup_table = builder.lookup.build_table(
+            data=move_rates_data,
             key_columns=["sex", "race_ethnicity"],
             parameter_columns=["age"],
-            value_columns=[
-                "gq_person_migration_rate",
-                "new_household_migration_rate",
-                "non_reference_person_migration_rate",
-            ],
+            value_columns=["gq_person", "new_household", "non_reference_person", "no_move"],
         )
-        self.person_move_rates = builder.value.register_rate_producer(
-            f"{self.name}.move_rates", source=move_rates_data
+        self.person_move_probabilities = builder.value.register_value_producer(
+            f"{self.name}.move_probabilities",
+            source=move_probabilities_lookup_table,
         )
 
         builder.event.register_listener("time_step", self.on_time_step)
@@ -81,27 +94,23 @@ class PersonMigration:
         Assigns those simulants relationship to head of household 'Other nonrelative'
         """
 
-        persons = self.population_view.get(event.index)
+        pop = self.population_view.get(event.index)
 
         # Get subsets of simulants that do each move type on this timestep
-        move_type_probabilities = (
-            self.person_move_rates(persons.index)
-            .rename(columns=lambda c: c.replace("_migration_rate", ""))
-            .assign(no_move=RESIDUAL_CHOICE)
-        )
+        move_type_probabilities = self.person_move_probabilities(pop.index)
         move_types_chosen = self.randomness.choice(
-            persons.index,
+            pop.index,
             choices=move_type_probabilities.columns,
             p=move_type_probabilities.values,
         )
 
-        domestic_movers_idx = persons.index[move_types_chosen != "no_move"]
+        domestic_movers_idx = pop.index[move_types_chosen != "no_move"]
 
         # TODO: Handle move types correctly -- this is code from the previous migration implementation
         # which only had a single type of individual move.
 
         # Get series of new household_ids the domestic_movers_idx will move to
-        new_households = self._get_new_household_ids(persons, domestic_movers_idx)
+        new_households = self._get_new_household_ids(pop, domestic_movers_idx)
 
         # get address and zipcode corresponding to selected households
         new_household_data = (
@@ -118,9 +127,9 @@ class PersonMigration:
         new_household_data_map = new_household_data.set_index("household_id")
 
         # update household data for domestic movers
-        persons.loc[domestic_movers_idx, "household_id"] = new_households
-        persons = update_address_and_zipcode(
-            df=persons,
+        pop.loc[domestic_movers_idx, "household_id"] = new_households
+        pop = update_address_and_zipcode(
+            df=pop,
             rows_to_update=domestic_movers_idx,
             id_key=new_households,
             address_map=new_household_data_map["address"],
@@ -128,39 +137,37 @@ class PersonMigration:
         )
 
         # update relation to head of household data
-        persons.loc[domestic_movers_idx, "relation_to_household_head"] = "Other nonrelative"
-        persons.loc[
-            (persons.index.isin(domestic_movers_idx))
+        pop.loc[domestic_movers_idx, "relation_to_household_head"] = "Other nonrelative"
+        pop.loc[
+            (pop.index.isin(domestic_movers_idx))
             & (
-                persons["household_id"].isin(
+                pop["household_id"].isin(
                     data_values.NONINSTITUTIONAL_GROUP_QUARTER_IDS.values()
                 )
             ),
             "relation_to_household_head",
         ] = "Noninstitutionalized GQ pop"
-        persons.loc[
-            (persons.index.isin(domestic_movers_idx))
+        pop.loc[
+            (pop.index.isin(domestic_movers_idx))
             & (
-                persons["household_id"].isin(
-                    data_values.INSTITUTIONAL_GROUP_QUARTER_IDS.values()
-                )
+                pop["household_id"].isin(data_values.INSTITUTIONAL_GROUP_QUARTER_IDS.values())
             ),
             "relation_to_household_head",
         ] = "Institutionalized GQ pop"
 
         # Update housing type
-        persons.loc[
-            (persons.index.isin(domestic_movers_idx))
-            & (persons["household_id"].isin(data_values.GQ_HOUSING_TYPE_MAP.keys())),
+        pop.loc[
+            (pop.index.isin(domestic_movers_idx))
+            & (pop["household_id"].isin(data_values.GQ_HOUSING_TYPE_MAP.keys())),
             "housing_type",
-        ] = persons["household_id"].map(data_values.GQ_HOUSING_TYPE_MAP)
-        persons.loc[
-            (persons.index.isin(domestic_movers_idx))
-            & (~persons["household_id"].isin(data_values.GQ_HOUSING_TYPE_MAP.keys())),
+        ] = pop["household_id"].map(data_values.GQ_HOUSING_TYPE_MAP)
+        pop.loc[
+            (pop.index.isin(domestic_movers_idx))
+            & (~pop["household_id"].isin(data_values.GQ_HOUSING_TYPE_MAP.keys())),
             "housing_type",
         ] = "Standard"
 
-        self.population_view.update(persons)
+        self.population_view.update(pop)
 
     ##################
     # Helper methods #
