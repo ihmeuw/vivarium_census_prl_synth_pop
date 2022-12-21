483cadce6cd2891561f7cf9e15efecd9b1452ba7
albrja <albrja@uw.edu>
2022-12-20 17:32:13 -0800

    Rebase and formatting

--- src/vivarium_census_prl_synth_pop/components/population.py
+++ src/vivarium_census_prl_synth_pop/components/population.py
@@ -10,10 +10,6 @@ from vivarium.framework.population import SimulantData
 from vivarium.framework.time import get_time_stamp
 from vivarium_public_health.utilities import DAYS_PER_YEAR, to_years
 
-from vivarium_census_prl_synth_pop.components.synthetic_pii import (
-    NameGenerator,
-    SSNGenerator,
-)
 from vivarium_census_prl_synth_pop.constants import data_keys, data_values, metadata
 from vivarium_census_prl_synth_pop.utilities import vectorized_choice
 
@@ -31,7 +27,6 @@ PARTNERS = [
 
 
 class Population:
-
     @property
     def name(self):
         return "population"
