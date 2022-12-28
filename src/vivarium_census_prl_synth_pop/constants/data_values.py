2e1198d41843bdab59f53caf4047769fae238fe3
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2022-12-28 10:52:29 -0800

    fix gq proportion test to work on small populations

--- src/vivarium_census_prl_synth_pop/constants/data_values.py
+++ src/vivarium_census_prl_synth_pop/constants/data_values.py
@@ -7,6 +7,7 @@ from typing import NamedTuple
 # TODO: implement gbd call (vivarium_inputs.get_population_structure("United States"))
 US_POPULATION = 333339776
 
+MAX_HOUSEHOLD_SIZE = 17
 PROP_POPULATION_IN_GQ = 0.03
 PROBABILITY_OF_TWINS = 0.04
 N_GROUP_QUARTER_TYPES = 6
