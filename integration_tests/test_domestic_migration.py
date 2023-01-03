f5058146d9b32f41a01cbe119ffacb450c8817b2
Zeb Burke-Conte <zmbc@uw.edu>
2023-01-03 11:03:25 -0800

    Change definition of new household moves

--- integration_tests/test_domestic_migration.py
+++ integration_tests/test_domestic_migration.py
@@ -17,22 +17,24 @@ def test_individuals_move_into_new_households(simulants_on_adjacent_timesteps):
         new_household_movers = (
             (before["household_id"] != after["household_id"])
             & (after["housing_type"] == "Standard")
-            & (after["relation_to_household_head"] == "Reference person")
+            & (~after["household_id"].isin(before["household_id"]))
         )
         assert new_household_movers.any()
 
-        # Household IDs moved to are unique and new
+        # Household IDs moved to are unique
         new_households = after[new_household_movers]["household_id"]
         assert new_households.nunique() == len(new_households)
-        assert not new_households.isin(before["household_id"]).any()
 
         # Address IDs moved to are unique and new
         new_addresses = after[new_household_movers]["address_id"]
         assert new_addresses.nunique() == len(new_addresses)
-        assert not before["address_id"].isin(new_addresses).any()
+        assert not new_addresses.isin(before["address_id"]).any()
 
         # Households are single-person
         assert not after[~new_household_movers]["household_id"].isin(new_households).any()
+        assert (
+            after[new_household_movers]["relation_to_household_head"] == "Reference person"
+        ).all()
 
 
 def test_households_move(simulants_on_adjacent_timesteps):
