aa0bd1dc7eadec815929978d40b9655fd00cf4bc
Zeb Burke-Conte <zmbc@users.noreply.github.com>
2023-01-03 12:08:19 -0800

    Test household moves (#136)

--- integration_tests/test_domestic_migration.py
+++ integration_tests/test_domestic_migration.py
@@ -77,6 +77,25 @@ def test_households_move(simulants_on_adjacent_timesteps):
         )
         assert household_movers.any()
 
+        # Household moves don't change household structure
+        assert (
+            before[household_movers]["relation_to_household_head"]
+            == after[household_movers]["relation_to_household_head"]
+        ).all()
+        assert (
+            before[household_movers]["housing_type"]
+            == after[household_movers]["housing_type"]
+        ).all()
+
+        # Address IDs moved to are new
+        moved_households = before[household_movers]["household_id"].unique()
+        new_addresses = after[household_movers]["address_id"]
+        assert new_addresses.nunique() == len(moved_households)
+        assert not before["address_id"].isin(new_addresses).any()
+
+        # Never GQ households
+        assert (before[household_movers]["housing_type"] == "Standard").all()
+
 
 def test_only_living_people_move(simulants_on_adjacent_timesteps):
     for before, after in simulants_on_adjacent_timesteps:
