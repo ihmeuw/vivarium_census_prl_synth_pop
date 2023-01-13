19bbc835ece4527b33e9ed6c25ce9ad717104b99
albrja <albrja@uw.edu>
2023-01-13 09:55:18 -0800

    Added additional check in test

--- integration_tests/test_household_structure.py
+++ integration_tests/test_household_structure.py
@@ -209,7 +209,7 @@ fake_population = pd.DataFrame(
 )
 
 
-def test_update_to_reference_person_and_relationships(mocker):
+def test_update_to_reference_person_and_relationships():
 
     expected_relationships = pd.Series(
         data=[
@@ -248,8 +248,17 @@ def test_update_to_reference_person_and_relationships(mocker):
     pop.reference_person_update_relationships_map = pd.read_csv(
         paths.REFERENCE_PERSON_UPDATE_RELATIONSHIP_DATA_PATH,
     )
-
+    # This is a series with household_id as the index and the new reference person as the value
+    expected_reference_person = (
+        fake_population.loc[fake_population.index].groupby(["household_id"])["age"].idxmax()
+    )
     updated_population = pop.update_reference_person_and_relationships(fake_population)
 
     assert not updated_population["relation_to_household_head"].isnull().any()
     assert (updated_population["relation_to_household_head"] == expected_relationships).all()
+
+    new_reference_person_idx = updated_population.index[
+        updated_population["relation_to_household_head"] == "Reference person"
+    ]
+
+    assert (expected_reference_person == new_reference_person_idx).all()
