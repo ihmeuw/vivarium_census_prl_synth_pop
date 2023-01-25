028102dc63eb40b7f554a401debbc8fd229f4e31
Zeb Burke-Conte <zmbc@users.noreply.github.com>
2023-01-25 12:40:28 -0800

    Ingest immigrant household weights (#169 and #173)

--- src/vivarium_census_prl_synth_pop/components/immigration.py
+++ src/vivarium_census_prl_synth_pop/components/immigration.py
@@ -25,15 +25,12 @@ class Immigration:
 
     def setup(self, builder: Builder):
         persons_data = builder.data.load(data_keys.POPULATION.PERSONS)
+        households_data = builder.data.load(data_keys.POPULATION.HOUSEHOLDS)
 
         immigrants = persons_data[persons_data["immigrated_in_last_year"]]
 
-        is_gq = immigrants["relation_to_household_head"].isin(
-            [
-                "Insitutionalized GQ pop",
-                "Noninstitutionalized GQ pop",
-            ]
-        )
+        gq_households = households_data[households_data["household_type"] != "Housing unit"]
+        is_gq = immigrants["census_household_id"].isin(gq_households["census_household_id"])
         self.gq_immigrants = immigrants[is_gq]
 
         non_gq_immigrants = immigrants[~is_gq]
@@ -47,3 +44,12 @@ class Immigration:
 
         self.household_immigrants = non_gq_immigrants[is_household_immigrant]
         self.non_reference_person_immigrants = non_gq_immigrants[~is_household_immigrant]
+
+        # Get the *household* (not person) weights for each household that can immigrate
+        # in a household move, for use in sampling.
+        self.immigrant_household_weights = households_data.set_index(
+            "census_household_id"
+        ).loc[
+            immigrant_reference_people["census_household_id"],
+            "household_weight",
+        ]
