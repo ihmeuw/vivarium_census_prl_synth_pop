828ebac8c5c9519e0ff49e15f88304b07cfb2dcf
Steve Bachmeier <sbachmei@uw.edu>
2023-02-13 15:34:09 -0800

    black

--- src/vivarium_census_prl_synth_pop/components/observers.py
+++ src/vivarium_census_prl_synth_pop/components/observers.py
@@ -711,7 +711,12 @@ class TaxW2Observer(BaseObserver):
 
         # merge in employer columns based on employer_id
         emp = business_details.groupby("employer_id").first()
-        for col in ["employer_address_id", "employer_state_id", "employer_puma", "employer_name"]:
+        for col in [
+            "employer_address_id",
+            "employer_state_id",
+            "employer_puma",
+            "employer_name",
+        ]:
             df_w2[col] = df_w2["employer_id"].map(emp[col])
 
         return df_w2[self.output_columns]
