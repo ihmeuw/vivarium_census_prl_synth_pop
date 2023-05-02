1de81b424f1b8ab8ee53cd911b894d2091eee636
albrja <albrja@uw.edu>
2023-05-01 18:02:12 -0700

    Rewrite nickname proportion

--- src/vivarium_census_prl_synth_pop/data/nicknames_proportion.py
+++ src/vivarium_census_prl_synth_pop/data/nicknames_proportion.py
@@ -5,21 +5,17 @@ from vivarium import Artifact
 from vivarium_census_prl_synth_pop.constants import data_keys, paths
 
 
-def _load_nicknames_data():
-    # Load and format nicknames dataset
-    nicknames = pd.read_csv(paths.NICKNAMES_DATA_PATH)
-    nicknames = nicknames.apply(lambda x: x.astype(str).str.title()).set_index("name")
-    nicknames = nicknames.replace("Nan", np.nan)
-    return nicknames
-
-
 def get_nicknames_proportion():
     # This function calculates the proportion of names that have potential nicknames. This will return a float that
     # that will be used as a constant to scale the nicknames noise function in the pseudopeople packages.
 
     art = Artifact(paths.DEFAULT_ARTIFACT)
     names = art.load(data_keys.SYNTHETIC_DATA.FIRST_NAMES).reset_index()["name"]
-    nicknames = _load_nicknames_data()
+    # Load and format nicknames dataset
+    names_with_nicknames = pd.read_csv(paths.NICKNAMES_DATA_PATH)
+    names_with_nicknames = names_with_nicknames["name"].apply(lambda x: x.astype(str).str.title())
 
-    proportion_of_nicknames = len(nicknames.index) / len(names)
+    # Proportion
+    nickname_eligible = names.isin(names_with_nicknames)
+    proportion_of_nicknames = nickname_eligible.sum() / len(names)
     return proportion_of_nicknames
