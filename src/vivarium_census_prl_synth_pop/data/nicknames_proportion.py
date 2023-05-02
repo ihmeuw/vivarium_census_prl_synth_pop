5e994ef02680ed7c7737d59246e7f18b4fdadc1d
albrja <albrja@uw.edu>
2023-05-02 12:15:02 -0700

    Fixing calculation of proportion

--- src/vivarium_census_prl_synth_pop/data/nicknames_proportion.py
+++ src/vivarium_census_prl_synth_pop/data/nicknames_proportion.py
@@ -9,12 +9,14 @@ def get_nicknames_proportion():
     # that will be used as a constant to scale the nicknames noise function in the pseudopeople packages.
 
     art = Artifact(paths.DEFAULT_ARTIFACT)
-    names = art.load(data_keys.SYNTHETIC_DATA.FIRST_NAMES).reset_index()["name"]
+    names = art.load(data_keys.SYNTHETIC_DATA.FIRST_NAMES).reset_index()
     # Load and format nicknames dataset
     names_with_nicknames = pd.read_csv(paths.NICKNAMES_DATA_PATH)
     names_with_nicknames = names_with_nicknames["name"].apply(lambda x: x.title())
 
     # Proportion
-    nickname_eligible = names.isin(names_with_nicknames)
-    proportion_of_nicknames = nickname_eligible.sum() / len(names)
+    nickname_eligible = names["name"].isin(names_with_nicknames)
+    proportion_of_nicknames = (
+        names.loc[nickname_eligible, "freq"].sum() / names.loc[:, "freq"].sum()
+    )
     return proportion_of_nicknames
