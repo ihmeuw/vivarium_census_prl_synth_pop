acf53f143af79ec24cb9e595ebe65f94217e1d13
albrja <albrja@uw.edu>
2023-10-27 16:42:43 -0700

    Remove index

--- src/vivarium_census_prl_synth_pop/tools/make_results.py
+++ src/vivarium_census_prl_synth_pop/tools/make_results.py
@@ -587,4 +587,4 @@ def write_shard_metadata(
 
     # Write shard metadata
     shard_metadata_path = obs_dir / f"shard_metadata_{seed}.csv"
-    shard_metadata.to_csv(shard_metadata_path)
+    shard_metadata.to_csv(shard_metadata_path, index=False)
