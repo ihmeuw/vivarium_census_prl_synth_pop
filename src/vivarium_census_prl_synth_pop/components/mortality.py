6543b4f17087bfacc05c5b8f3faa92eeb569823f
Zeb Burke-Conte <zmbc@users.noreply.github.com>
2023-10-27 14:32:48 -0700

    Rename updated_relationship_to_reference_person (#332)

--- src/vivarium_census_prl_synth_pop/components/mortality.py
+++ src/vivarium_census_prl_synth_pop/components/mortality.py
@@ -30,4 +30,6 @@ class Mortality(_Mortality):
         new_relationship_to_reference_person = self.updated_relationship_to_reference_person(
             event.index
         )
-        self.population_view.update(new_relationship_to_reference_person)
+        self.population_view.update(
+            new_relationship_to_reference_person.rename("relationship_to_reference_person")
+        )
