3c31f6bf47984e0caf95db56625126bc3be59979
albrja <albrja@uw.edu>
2024-01-17 15:01:15 -0800

    Rename test

--- tests/test_read_file.py
+++ tests/test_read_file.py
@@ -4,5 +4,5 @@ from vivarium import Artifact
 from vivarium_census_prl_synth_pop.constants.paths import DEFAULT_ARTIFACT
 
 
-def read_artifact():
+def test_read_artifact():
     assert DEFAULT_ARTIFACT.exists()
