540474b5b880468d6e48856d8a5fa397e6b94e34
James Albright <albrja@uw.edu>
2024-02-01 11:30:15 -0800

    Unpin vivarium

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         long_description = f.read()
 
     install_requirements = [
-        "vivarium>=2.0.0, <2.3.0",
+        "vivarium",
         "vivarium_public_health>=2.0.0",
         "click",
         "gbd_mapping>=3.0.6, <4.0.0",
